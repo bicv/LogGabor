3ba57fb7996ee8b1a8db18ace8aedb6c4b27d798
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-04-02 22:35:27 +0200

    changed params

--- default_param.py
+++ default_param.py
@@ -1,23 +1,32 @@
 # -*- coding: utf8 -*-
 {
 # Image
+# 'N_image' : None, #use all images in the folder
+'N_image' : 100, #use 100 images in the folder
+# 'N_image' : 10, #use 4 images in the folder
+'seed' : None, # a seed for the Random Number Generator (RNG) for picking images in databases, set to None xor set to a given number to freeze the RNG
 'N_X' : 256, # size of images
 'N_Y' : 256, # size of images
-'do_whitening'  : True, # = self.pe.do_whitening
+# 'N_X' : 64, # size of images
+# 'N_Y' : 64, # size of images
+'noise' : 0.1, # level of noise when we use some
 'do_mask'  : True, # self.pe.do_mask
-'seed': None, # a seed for the Random Number Generator (RNG) for picking images in databases, set to None or a given number to freeze the RNG
+# whitening parameters:
+'do_whitening'  : True, # = self.pe.do_whitening
+'white_name_database' : 'serre07_distractors',
+'white_n_learning' : 0,
+'white_N' : .07,
+'white_N_0' : .0, # olshausen = 0.
+'white_f_0' : .4, # olshausen = 0.2
+'white_alpha' : 1.4,
+'white_steepness' : 4.,
+'white_recompute' : False,
 # Log-Gabor
 #'base_levels' : 2.,
 'base_levels' : 1.618,
 'n_theta' : 24, # number of (unoriented) angles between 0. radians (included) and np.pi radians (excluded)
-'B_sf' : 1.5, # 1.5 in Geisler
-'B_theta' : 3.14159/8.,
-'figpath' : 'figures/',
-'edgefigpath' : 'figures/edges/',
-'matpath' : 'mat/',
-'edgematpath' : 'mat/edges/',
-'datapath' : 'database/',
-'ext' : '.pdf',
+'B_sf' : .4, # 1.5 in Geisler
+'B_theta' : 3.14159/18.,
 # EdgeFactory PARAMETERS
 'd_width' : 45., # Geisler 1.23 deg (full image = 45deg)
 'd_min' : .25, # Geisler 1.23 deg (full image = 45deg)
@@ -27,6 +36,11 @@
 'N_phi' : 12, #
 'N_scale' : 5, #
 'loglevel_max': 7,
-'N_image' : None, #use all images in the folder 200, #None
-'noise' : 0.5, #
+# PATHS
+'figpath' : 'figures/',
+'edgefigpath' : 'figures/edges/',
+'matpath' : 'mat/',
+'edgematpath' : 'mat/edges/',
+'datapath' : 'database/',
+'ext' : '.pdf',
 }
