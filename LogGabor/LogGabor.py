f660dd5cb25dee1249d52b75127becba486916a2
Laurent Perrinet <laurent.perrinet@gmail.com>
2016-12-08 15:13:12 +0100

    moving pyramid

--- LogGabor/LogGabor.py
+++ LogGabor/LogGabor.py
@@ -23,6 +23,69 @@ class LogGabor(Image):
         Image.__init__(self, pe)
         self.init_logging(name='LogGabor')
 
+    ## PYRAMID
+    def init(self):
+        Image.init(self)
+
+        self.n_levels = int(np.log(np.max((self.pe.N_X, self.pe.N_Y)))/np.log(self.pe.base_levels))
+        self.sf_0 = 1. / np.logspace(1, self.n_levels, self.n_levels, base=self.pe.base_levels)
+        self.theta = np.linspace(-np.pi/2, np.pi/2, self.pe.n_theta+1)[1:]
+
+        self.oc = (self.pe.N_X * self.pe.N_Y * self.pe.n_theta * self.n_levels) #(1 - self.pe.base_levels**-2)**-1)
+
+    def linear_pyramid(self, image):
+        C = np.empty((self.pe.N_X, self.pe.N_Y, self.pe.n_theta, self.n_levels), dtype=np.complex)
+        for i_sf_0, sf_0 in enumerate(self.sf_0):
+            for i_theta, theta in enumerate(self.theta):
+                FT_lg = self.loggabor(0, 0, sf_0=sf_0, B_sf=self.pe.B_sf,
+                                    theta=theta, B_theta=self.pe.B_theta)
+                C[:, :, i_theta, i_sf_0] = self.FTfilter(image, FT_lg, full=True)
+        return C
+
+    def golden_pyramid(self, z):
+        """
+        The Golden Laplacian Pyramid.
+        To represent the edges of the image at different levels, we may use a simple recursive approach constructing progressively a set of images of decreasing sizes, from a base to the summit of a pyramid. Using simple down-scaling and up-scaling operators we may approximate well a Laplacian operator. This is represented here by stacking images on a Golden Rectangle, that is where the aspect ratio is the golden section $\phi \eqdef \frac{1+\sqrt{5}}{2}$. We present here the base image on the left and the successive levels of the pyramid in a clockwise fashion (for clarity, we stopped at level $8$). Note that here we also use $\phi^2$ (that is $\phi+1$) as the down-scaling factor so that the resolution of the pyramid images correspond across scales. Note at last that coefficient are very kurtotic: most are near zero, the distribution of coefficients has long tails.
+
+        """
+        import matplotlib.pyplot as plt
+
+        phi = (np.sqrt(5) +1.)/2. # golden ratio
+        opts= {'vmin':0., 'vmax':1., 'interpolation':'nearest', 'origin':'upper'}
+        fig_width = 13
+        fig = plt.figure(figsize=(fig_width, fig_width/phi))
+        xmin, ymin, size = 0, 0, 1.
+        axs = []
+        for i_sf_0, sf_0_ in enumerate(self.sf_0):
+            ax = fig.add_axes((xmin/phi, ymin, size/phi, size), axisbg='w')
+            ax.axis(c='b', lw=0)
+            plt.setp(ax, xticks=[], yticks=[])
+            im_RGB = np.zeros((self.pe.N_X, self.pe.N_Y, 3))
+            for i_theta, theta_ in enumerate(self.theta):
+                im_abs = np.absolute(z[:, :, i_theta, i_sf_0])
+                RGB = np.array([.5*np.sin(2*theta_ + 2*i*np.pi/3)+.5 for i in range(3)])
+                im_RGB += im_abs[:,:, np.newaxis] * RGB[np.newaxis, np.newaxis, :]
+
+            im_RGB /= im_RGB.max()
+            ax.imshow(im_RGB, **opts)
+            #a.grid(False)
+            ax.grid(b=False, which="both")
+            i_orientation = np.mod(i_sf_0, 4)
+            if i_orientation==0:
+                xmin += size
+                ymin += size/phi**2
+            elif i_orientation==1:
+                xmin += size/phi**2
+                ymin += -size/phi
+            elif i_orientation==2:
+                xmin += -size/phi
+            elif i_orientation==3:
+                ymin += size
+            axs.append(ax)
+            size /= phi
+
+        return fig, axs
+
     ## LOW LEVEL OPERATIONS
     def band(self, sf_0, B_sf):
         # selecting a donut (the ring around a prefered frequency)
@@ -76,6 +139,8 @@ class LogGabor(Image):
         FT_lg = self.loggabor(u, v, sf_0, B_sf, theta, B_theta)
         fig, a1, a2 = self.show_FT(FT_lg * np.exp(-1j*phase))
         return fig, a1, a2
+
+
 
 def _test():
     import doctest
