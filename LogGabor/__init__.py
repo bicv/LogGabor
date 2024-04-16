32d7e2f8d54f13580652070574996309e488e40d
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2024-04-16 15:29:46 +0200

    sync

--- LogGabor/__init__.py
+++ LogGabor/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import division, print_function, absolute_import
 __author__ = "Laurent Perrinet INT - CNRS"
-__version__ = '20240420'
+__version__ = '20240419'
 __licence__ = 'GPLv2'
 from SLIP import imread
 from .LogGabor import LogGabor, LogGaborFit
